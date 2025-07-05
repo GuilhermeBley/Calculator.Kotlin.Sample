eb64d1bf7be01397764edc17d0a5cfd1cafd701b
Guilherme Bley <guilhermecbley@gmail.com>
2025-07-04 22:47:55 -0300

    Fixing project version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.calculatorkotlinsample"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.calculatorkotlinsample"
